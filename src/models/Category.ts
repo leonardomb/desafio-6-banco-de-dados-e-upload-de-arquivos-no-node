bce312da44d04166f5cc28f5790cbc9c07d60468
“leonardomborges” <leonardo@neppo.com.br>
2020-07-03 07:58:30 -0300

    List categories with transactions

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -2,14 +2,19 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  OneToMany,
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import Transaction from './Transaction';
 
 @Entity('categories')
 class Category {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 
   @Column()
   title: string;
